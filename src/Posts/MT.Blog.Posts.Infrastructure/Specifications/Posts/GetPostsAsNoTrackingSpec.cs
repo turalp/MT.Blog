a1541d21e5ec9e9ede820de309fa12bb1403417d
Tural <tural.mamedov@outlook.com>
2024-10-05 09:30:06 +0300

    Added include, fixed admin page for posts

--- src/Posts/MT.Blog.Posts.Infrastructure/Specifications/Posts/GetPostsAsNoTrackingSpec.cs
+++ src/Posts/MT.Blog.Posts.Infrastructure/Specifications/Posts/GetPostsAsNoTrackingSpec.cs
@@ -5,7 +5,7 @@ namespace MT.Blog.Posts.Infrastructure.Specifications.Posts;
 
 public sealed class GetPostsAsNoTrackingSpec : Specification<Post>, ISpecification<Post>
 {
-    private GetPostsAsNoTrackingSpec() => Query.AsNoTracking();
+    private GetPostsAsNoTrackingSpec() => Query.AsNoTracking().Include(p => p.ParentPost).Include(p => p.Category);
 
     public static GetPostsAsNoTrackingSpec Create() => new();
 }
